b3279ba4f65e1bbfea6e61dc1e34a2800a8e7e4f
lucasdav <lucassouzadavanso@gmail.com>
2024-06-26 23:06:08 -0300

    aula 05

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StockModule } from './stock/stock.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/pettech'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     StockModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '10m' },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
